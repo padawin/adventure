1fc6df9edf7366049727e1cad784fe62615c12a9
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-04-24 21:23:50 +0100

    a story has now a content and a title

--- stories/test.js
+++ stories/test.js
@@ -1,25 +1,28 @@
 if (!stories)var stories={};
 
 stories['test'] = {
-	'_start_': {
-		'description': 'Type start to begin your journey...<br /><br />',
-		'links': {
-			'start': 'scene1'
+	'title': 'A test story...',
+	'content': {
+		'_start_': {
+			'description': 'Type start to begin your journey...<br /><br />',
+			'links': {
+				'start': 'scene1'
+			}
+		},
+		'scene1': {
+			'description': 'Scene 1 description.<br/>2 for scene 2; 3 for scene 3<br/>',
+			'links': {
+				'2': 'scene2',
+				'3': 'scene3'
+			}
+		},
+		'scene2': {
+			'description': 'End with scene 2',
+			'links': {}
+		},
+		'scene3': {
+			'description': 'End with scene 3',
+			'links': {}
 		}
-	},
-	'scene1': {
-		'description': 'Scene 1 description.<br/>2 for scene 2; 3 for scene 3<br/>',
-		'links': {
-			'2': 'scene2',
-			'3': 'scene3'
-		}
-	},
-	'scene2': {
-		'description': 'End with scene 2',
-		'links': {}
-	},
-	'scene3': {
-		'description': 'End with scene 3',
-		'links': {}
 	}
 };
