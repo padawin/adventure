59a86749ce08365e418261812a20e24e08d47d85
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-04-24 20:11:24 +0100

    form used to validate with the keyboard

--- js/engine.js
+++ js/engine.js
@@ -4,13 +4,13 @@
  * @param story The story to play
  * @param screen The DOM element where the text must be displayed
  * @param input The DOM element where the player will type his choices
- * @param button The DOM element where the player will validate his choices
+ * @param form The DOM element of the form to enter the action
  * @param scene Placeholder, the current scene to play
  * @param updateScreen Placeholder, function to update the screen with the new
  * 		scene
  * @param newSceneIndex Placeholder, index of the next scene to play
  */
-var engine=function(story,screen,input,button,scene,updateScreen,newSceneIndex){
+var engine=function(story,screen,input,form,scene,updateScreen,newSceneIndex){
 	// Set the initial scene
 	scene=story[story._start_];
 	// define the function to update the screen with a scene description
@@ -18,7 +18,7 @@ var engine=function(story,screen,input,button,scene,updateScreen,newSceneIndex){
 		screen.innerHTML+=scene.description;
 	}
 	// event to validate the user's choice
-	button.onclick=function(){
+	form.onsubmit=function(){
 		// get the chosen scene's index
 		newSceneIndex=scene.links[input.value];
 		if(newSceneIndex){
@@ -26,6 +26,7 @@ var engine=function(story,screen,input,button,scene,updateScreen,newSceneIndex){
 			scene=story[newSceneIndex];
 			updateScreen();
 		}
+		return false;
 	}
 
 	// initial screen update
