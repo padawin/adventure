eba12b4327f2bf4dac4bc2bd33ca3aab0013cc26
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-11-16 17:19:05 +0000

    submit event moved out of the engine, it must be with the gui

--- js/engine.js
+++ js/engine.js
@@ -4,30 +4,29 @@
  * @param story The story to play
  * @param screen The DOM element where the text must be displayed
  * @param input The DOM element where the player will type his choices
- * @param form The DOM element of the form to enter the action
  * @param scene Placeholder, the current scene to play
  * @param updateScreen Placeholder, function to update the screen with the new
  * 		scene
  * @param newSceneIndex Placeholder, index of the next scene to play
  */
-var engine=function(story,screen,input,form,scene,updateScreen,newSceneIndex,value){
+var engine=function(story,screen,input,scene,updateScreen,newSceneIndex,value){
 	value='value';
 	// Set the initial scene
 	scene=story._start_;
 	// define the function to update the screen with a scene description
 	updateScreen=function(){
 		screen.innerHTML+=scene.description;
-	}
+	};
+
+	// initial screen update
+	updateScreen();
+
 	// event to validate the user's choice
-	form.onsubmit=function(){
+	return function(){
 		// get the chosen scene's index
 		newSceneIndex=scene.links[input[value]];
 		input[value]='';
 		input.focus();
 		newSceneIndex && updateScreen(scene=story[newSceneIndex]);
-		return !1;
 	}
-
-	// initial screen update
-	updateScreen();
 };
